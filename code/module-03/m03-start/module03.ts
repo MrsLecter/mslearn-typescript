dafb4497cb07c823df21e53e3a755d45bcf6fd91
MrsLecter <MissisLecter@protonmail.com>
2021-06-28 21:37:11 +0300

    fixed: module04, module05

--- code/module-03/m03-start/module03.ts
+++ code/module-03/m03-start/module03.ts
@@ -3,34 +3,34 @@
 
 /*  EXERCISE 1
     TODO: Declare the Loan interface. */
+interface Loan {
+    principle: number;
+    interestRate: number;
+}
 
+interface ConventionalLoan extends Loan {
+    months: number
+}
 
-/*  TODO: Declare the ConventionalLoan interface. */
-
-
-
-/*  TODO: Update the calculateInterestOnlyLoanPayment function. */
-
-function calculateInterestOnlyLoanPayment(principle, interestRate) {
+function calculateInterestOnlyLoanPayment(Loan: any): string {
     // Calculates the monthly payment of an interest only loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
-    let payment;
-    payment = principle * interest;
+    let interest: number;
+    interest = Loan.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let payment: number;
+    payment = Loan.principle * interest;
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 
-/*  TODO: Update the calculateConventionalLoanPayment function. */
-
-function calculateConventionalLoanPayment(principle, interestRate, months) {
-    // Calculates the monthly payment of a conventional loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
-    let payment;
-    payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
+function calculateConventionalLoanPayment(ConventionalLoan: any): string {
+    let interest: number;
+    interest = ConventionalLoan.interestRate / 1200;
+    let payment: number;
+    payment = ConventionalLoan.principle * interest / (1 - (Math.pow(1 / (1 + interest), ConventionalLoan.months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
 
-let interestOnlyPayment = calculateInterestOnlyLoanPayment(30000, 5);
-let conventionalPayment = calculateConventionalLoanPayment(30000, 5, 180);
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({ principle: 30000, interestRate: 5 });
+let conventionalPayment = calculateConventionalLoanPayment({ principle: 30000, interestRate: 5, months: 180 });
 
 console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
-console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
+console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24"
--- code/module-04/m04-start/module04.ts
+++ code/module-04/m04-start/module04.ts
@@ -1,70 +1,65 @@
 /* Module 4: Develop typed functions using TypeScript
    Lab Start  */
 
-/*  EXERCISE 1
-    TODO: Declare a new function type for the sortDescending and sortAscending functions. */
-    
+/*  EXERCISE 1 */
 
-/*  TODO: Convert the sortDescending and sortAscending functions to arrow 
-    functions. */
-
+type AscDesc = (a: number, b: number) => number;
 /*  sortDescending is a comparison function that tells the sort method how to sort 
     numbers in descending order */
-function sortDescending(a, b) {
-if (a > b) {
-    return -1;
-} else if (b > a) {
-    return 1;
-} else {
-    return 0;
-}
+let sortDescending: AscDesc = (a, b) => {
+    if (a > b) {
+        return -1;
+    } else if (b > a) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 /*  sortDescending is a comparison function that tells the sort method how to sort 
     numbers in ascending order. */
-function sortAscending(a, b) {
-if (a > b) {
-    return 1;
-} else if (b > a) {
-    return -1;
-} else {
-    return 0;
-}
+let sortAscending: AscDesc = (a, b) => {
+    if (a > b) {
+        return 1;
+    } else if (b > a) {
+        return -1;
+    } else {
+        return 0;
+    }
 }
 
 /*  The buildArray function builds an array of unique random numbers containing the number 
     of items based on the number passed to it. The sortOrder parameter determines 
     whether to sort the array in ascending or descending order. */
 
-/*  TODO: Update the BuildArray function. */
-
-function buildArray(items, sortOrder) {
-    let randomNumbers = [];
-    let nextNumber;
+function buildArray(items: number, sortOrder: 'ascending' | 'descending'): Array<number> {
+    let randomNumbers: Array<number> = [];
+    let nextNumber: number;
     for (let counter = 0; counter < items; counter++) {
         nextNumber = Math.ceil(Math.random() * (100 - 1));
         if (randomNumbers.indexOf(nextNumber) === -1) {
-          randomNumbers.push(nextNumber);
+            randomNumbers.push(nextNumber);
         } else {
-          counter--;
+            counter--;
         }
     }
     if (sortOrder === 'ascending') {
-      return randomNumbers.sort(sortAscending);
+        return randomNumbers.sort(sortAscending);
     } else {
-      return randomNumbers.sort(sortDescending);
+        return randomNumbers.sort(sortDescending);
     }
 }
 
 let myArray1 = buildArray(12, 'ascending');
 let myArray2 = buildArray(8, 'descending');
 
-/*  EXERCISE 2
-    TODO: Update the LoanCalculator function. */
+/*  EXERCISE 2 
+    Convert a JavaScript function to a TypeScript using optional parameters*/
 
-function loanCalculator (principle, interestRate, months) {
+
+function loanCalculator(principle: number, interestRate: number, months: number = 1): string {
     let interest = interestRate / 1200;   // Calculates the monthly interest rate
-    let payment;
-    payment = principle * interest / (1 - (Math.pow(1/(1 + interest), months)));
+    let payment: number;
+    payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
     return payment.toFixed(2);
 }
